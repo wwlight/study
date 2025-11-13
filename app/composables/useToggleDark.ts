3c4312fd6ad2ba2cda5100a2832bfac9ceb91e33
wwlight <wwlight8@qq.com>
2025-11-13 20:48:44 +0800

    chore: update deps

--- app/composables/useToggleDark.ts
+++ app/composables/useToggleDark.ts
@@ -34,6 +34,7 @@ export default function (event?: MouseEvent) {
       {
         duration: 400,
         easing: 'ease-in',
+        fill: 'both',
         pseudoElement: isDark.value
           ? '::view-transition-old(root)'
           : '::view-transition-new(root)',
